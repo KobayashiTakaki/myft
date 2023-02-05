0bee31743283eb635a3fe7e488d3d12799328c0c
kobayashitakaki <o.share.thief@gmail.com>
2023-02-05 21:52:44 +0900

    add ERC20 contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import * as dotenv from 'dotenv'
+dotenv.config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    localhost: {
+      url:"http://127.0.0.1:8545/",
+      chainId:31337,
+    },
+    shibuya: {
+      url: "https://evm.shibuya.astar.network",
+      chainId:81,
+      accounts: [
+        `0x${process.env.PRIVATE_KEY}`,
+      ],
+    },
+  }
 };
 
 export default config;
